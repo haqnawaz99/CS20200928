f22925346bb28eaadc579f52816962cb5275b922
Haq Nawaz <haqnawaz99@yahoo.com>
2020-09-28 13:51:14 +0500

    Button Functionality Added

--- app/src/main/java/org/haqnawaz/cs20200928/MainActivity.java
+++ app/src/main/java/org/haqnawaz/cs20200928/MainActivity.java
@@ -3,6 +3,8 @@ package org.haqnawaz.cs20200928;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,8 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    public void ShowMessage(View view) {
+
+        EditText editText = findViewById(R.id.editText);
         TextView textView = findViewById(R.id.txtViewMessage);
-        textView.setText("Is text changed");
+
+        textView.setText(editText.getText().toString());
     }
 }
