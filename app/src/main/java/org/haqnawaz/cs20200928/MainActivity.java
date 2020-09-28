1b96350f209ea4a598b50d723a3bfac01479093b
Haq Nawaz <haqnawaz99@yahoo.com>
2020-09-28 13:31:26 +0500

    Button Added

--- app/src/main/java/org/haqnawaz/cs20200928/MainActivity.java
+++ app/src/main/java/org/haqnawaz/cs20200928/MainActivity.java
@@ -3,6 +3,7 @@ package org.haqnawaz.cs20200928;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.txtViewMessage);
+        textView.setText("Is text changed");
     }
 }
